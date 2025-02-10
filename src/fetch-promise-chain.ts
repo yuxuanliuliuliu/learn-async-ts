3277f34ff224183863aca51054cde05a97018a57
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2025-02-10 09:44:36 -0500

    added clarification comments

--- src/fetch-promise-chain.ts
+++ src/fetch-promise-chain.ts
@@ -1,3 +1,4 @@
+// A type of the JSON data returned by the Products API
 interface Product {
   name: string;
   price: number;
@@ -5,10 +6,18 @@ interface Product {
   type: string;
 }
 
+// Call to fetch() returns a promise
 const fetchPromise4 = fetch(
     "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
   );
-  
+
+/**
+ * The promise is resolved when the response is received
+ * The json from the response is retrieved asynchronously
+ * Once the json is received, the promise is resolved
+ * The resolved promise is handled by logging the product names
+ * This is an illustration of refactoring callback hell by chaining promises
+ */
   fetchPromise4
     .then((response: Response) => response.json() as Promise<Product[]>)
     .then((products: Product[]) => {
--- src/fetch-promise-results.ts
+++ src/fetch-promise-results.ts
@@ -1,3 +1,4 @@
+// A type of the JSON data returned by the Products API
 interface Product {
   name: string;
   price: number;
@@ -5,10 +6,20 @@ interface Product {
   type: string;
 }
 
+/** Call to fetch() returns a promise 
+which is resolved when the response is received
+**/
 const fetchPromise2: Promise<Response> = fetch(
   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
 );
 
+/**
+ * The promise is resolved when the response is received
+ * The json from the response is retrieved asynchronously
+ * Once the json is received, the promise is resolved
+ * The resolved promise is handled by logging the product names
+ * This is an illustration of callback hell
+ */
 fetchPromise2.then((response: Response) => {
   const jsonPromise = response.json() as Promise<Product[]>;
   jsonPromise.then((products: Product[]) => {
@@ -18,6 +29,5 @@ fetchPromise2.then((response: Response) => {
   });
 });
 
-
-
+// Logging a message to indicate fetching has started
 console.log('Fetching products ... ');
