bed001972446640eda846a7a70d256cc1ba3e71a
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2025-02-10 11:04:11 -0500

    changed quiz

--- src/quiz/promise-2darray.ts
+++ src/quiz/promise-2darray.ts
@@ -1,9 +1,21 @@
+/**
+ * An asynchronous function that sums all numbers in a 2D array
+ * @param arr 2D array of numbers
+ * @returns a promise that resolves to the sum of all numbers in the 2D array
+ * or rejects if the array is empty
+ */
 function sum2DArray(arr: number[][]): Promise<number> {
     return new Promise((resolve, reject) => {
         console.log('Sum called ... ');
         if(arr.length === 0) {
             reject('Cannot sum an empty array');
         }
+        /** schedule the execution of the function to the next event loop cycle.
+         * This is done using setTimeout() to simulate an asynchronous operations.
+         * 
+         * Replace the logic in the setTimeout() with the actual logic to sum the numbers
+         * to understand the difference in execution with and without setTimeout()
+         **/
         setTimeout(() => {
             let sum = 0;
             for (let i = 0; i < arr.length; i++) {
