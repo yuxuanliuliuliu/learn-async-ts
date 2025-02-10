8f5d851cac349abfefa8386636f35e4fa34146c5
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2025-02-10 09:50:48 -0500

    added comments for error handling

--- src/fetch-promise-error.ts
+++ src/fetch-promise-error.ts
@@ -1,29 +1,38 @@
+// a type for the products from the API
 interface Product {
-    name: string;
-    price: number;
-    image: string;
-    type: string;
-  }
+  name: string;
+  price: number;
+  image: string;
+  type: string;
+}
 
+// Call to fetch() throws an error due to a bad URL
 const fetchPromise3 = fetch(
-    "https://mdn.github.io/learningarea/javascript/apis/fetching-data/can-store/products.json",
-  );
-  
-  fetchPromise3
-    .then((response: Response) => {
-        if(response.ok) {
-            return response.json() as Promise<Product[]>;
-        }
-        throw new Error(`Bad Response : ${response.status}`) 
-    })
-    .then((products: Product[]) => {
-        products.forEach((product: Product) => {
-            console.log(product.name);
-          });
-        })
-    .catch((error) => {
-        console.error(`Failed to get products: ${error}`);
-    });
+  "https://mdn.github.io/learningarea/javascript/apis/fetching-data/can-store/products.json",
+);
 
-    console.log('Chaining promises ... ');
-  
+/**
+ * The promise is resolved when the response is received
+ * The json from the response is retrieved asynchronously
+ * Once the json is received, the promise is resolved
+ * The resolved promise is handled by logging the product names
+ * If the response is not ok, an error is thrown
+ * This is an illustration of error handling
+ */
+fetchPromise3
+  .then((response: Response) => {
+    if (response.ok) {
+      return response.json() as Promise<Product[]>;
+    }
+    throw new Error(`Bad Response : ${response.status}`)
+  })
+  .then((products: Product[]) => {
+    products.forEach((product: Product) => {
+      console.log(product.name);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to get products: ${error}`);
+  });
+
+console.log('Chaining promises ... ');
